013b8fb17207eb2891ea64c06f229d95b1181f17
monirerahmani-dev <monirerahmani-dev@gmail.com>
2024-04-19 09:45:41 +0100

    provisioning EKS cluster with Terraform

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -4,12 +4,22 @@ module "eks" {
 
   cluster_name = "myapp-eks-cluster"
   cluster_version = "1.27"
+  cluster_endpoint_public_access = true
 
   subnet_ids = module.myapp-vpc.private_subnets
   vpc_id = module.myapp-vpc.vpc_id
-  
   tags = {
     environment = "development"
     application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t2.small"]
+    }
   }
 }
